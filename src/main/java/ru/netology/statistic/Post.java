fcd4bebbad88c0d9d2a6da7c56f85c70c2be789c
InnaB <isbocha@gmail.com>
2023-06-25 13:09:44 +0300

    Initial commit

---
+++ src/main/java/ru/netology/statistic/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.statistic;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для серии и номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // согласие о получении новостей о скидках в виде sms
+    public FormDate birthday;
+}
